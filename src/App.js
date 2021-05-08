fb2a426dbb4efac222bc37401c3a5c55bb1684f7
TERENCE <flagada08@gmail.com>
2021-05-08 17:44:12 +0200

    catch(error) disabled

--- src/App.js
+++ src/App.js
@@ -3,25 +3,25 @@ import Pages from './pages';
 
 function App() {
   const [user, setUser] = useState(null);
-  const [error, setError] = useState(null)
+  // const [error, setError] = useState(null)
   useEffect(() => {
     fetch('https://gitconnected.com/v1/portfolio/flagada08')
     .then(res => res.json())
     .then(user => {
       setUser(user);
     })
-    .catch(error => {
-      setError(error)
-    });
+    // .catch(error => {
+    //   setError(error)
+    // });
   }, []);
 
   if (!user) {
     return <div />;
   }
 
-  if (error) { 
-    return <Pages user={user} />;
-  }
+  // if (error) { 
+  //   return <Pages user={user} />;
+  // }
 
   return <Pages user={user} />;
 }
